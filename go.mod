468cf94af08160f4d16b13344e6b369eabdc63a6
Bilal <bilal.tehseen@avanzainnovations.com>
2022-11-18 01:27:14 +0500

    Inital Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bilaltahseen/go-lang/demo-1
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bilaltahseen/go-lang/demo-1/src/hello"
+)
+
+func main() {
+	fmt.Println(hello.Greeting())
+}
